e3683ef1ae63239a111ca6ab5e71491f9449f582
Diego Giraldo <diegoagg95@gmail.com>
2020-09-20 16:05:43 -0500

    initial commit

---
+++ GorillaPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorillaPost
+//
+//  Created by Diego Giraldo Gómez on 20/09/20.
+//  Copyright © 2020 Diego Giraldo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
